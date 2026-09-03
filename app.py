a101f9d9a609b03289a6a9a1d64eeffe1cecca4b
Aditya Walke <adityawalke97@gmail.com>
2026-09-03 11:28:32 +0530

    Refactor SQL insert statement formatting

--- app.py
+++ app.py
@@ -743,32 +743,30 @@ def submit_mock_test():
             )
 
         category = session.get("mock_category", "General")
-
-cursor.execute("""
-INSERT INTO results
-(
-    user_id,
-    score,
-    total_questions,
-    percentage,
-    category
-)
-VALUES
-(
-    %s,
-    %s,
-    %s,
-    %s,
-    %s
-)
-""", (
-    user_id,
-    score,
-    total_questions,
-    percentage,
-    category
-))
-
+        cursor.execute("""
+        INSERT INTO results
+        (
+        user_id,
+        score,
+        total_questions,
+        percentage,
+        category
+        )
+        VALUES
+        (
+        %s,
+        %s,
+        %s,
+        %s,
+        %s
+        )
+        """, (
+        user_id,
+        score,
+        total_questions,
+        percentage,
+        category
+            ))
         conn.commit()
 
         
